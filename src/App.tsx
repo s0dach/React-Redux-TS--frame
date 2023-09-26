28caf098588c1e9189df95078fbcb67355491015
Nikita <nsidukov25@gmail.com>
2023-09-26 21:49:12 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from "./components/UserList";
+import TodoList from "./components/TodoList";
+
+function App() {
+    return (
+        <div>
+            <UserList/>
+            <TodoList/>
+        </div>
+    );
+}
+
+export default App;
